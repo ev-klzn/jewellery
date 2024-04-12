60a97d049083d0ea8b57fdcfe740838a665a6d5b
Yevhen K <zhekan89@gmail.com>
2024-04-12 11:14:22 +0300

    save

--- js/app.js
+++ js/app.js
@@ -396,61 +396,6 @@
             }
         }
     }
-    class MousePRLX {
-        constructor(props, data = null) {
-            let defaultConfig = {
-                init: true,
-                logging: true
-            };
-            this.config = Object.assign(defaultConfig, props);
-            if (this.config.init) {
-                const paralaxMouse = document.querySelectorAll("[data-prlx-mouse]");
-                if (paralaxMouse.length) {
-                    this.paralaxMouseInit(paralaxMouse);
-                    this.setLogging(`Прокинувся, стежу за об'єктами: (${paralaxMouse.length})`);
-                } else this.setLogging("Немає жодного обєкта. Сплю...");
-            }
-        }
-        paralaxMouseInit(paralaxMouse) {
-            paralaxMouse.forEach((el => {
-                const paralaxMouseWrapper = el.closest("[data-prlx-mouse-wrapper]");
-                const paramСoefficientX = el.dataset.prlxCx ? +el.dataset.prlxCx : 100;
-                const paramСoefficientY = el.dataset.prlxCy ? +el.dataset.prlxCy : 100;
-                const directionX = el.hasAttribute("data-prlx-dxr") ? -1 : 1;
-                const directionY = el.hasAttribute("data-prlx-dyr") ? -1 : 1;
-                const paramAnimation = el.dataset.prlxA ? +el.dataset.prlxA : 50;
-                let positionX = 0, positionY = 0;
-                let coordXprocent = 0, coordYprocent = 0;
-                setMouseParallaxStyle();
-                if (paralaxMouseWrapper) mouseMoveParalax(paralaxMouseWrapper); else mouseMoveParalax();
-                function setMouseParallaxStyle() {
-                    const distX = coordXprocent - positionX;
-                    const distY = coordYprocent - positionY;
-                    positionX += distX * paramAnimation / 1e3;
-                    positionY += distY * paramAnimation / 1e3;
-                    el.style.cssText = `transform: translate3D(${directionX * positionX / (paramСoefficientX / 10)}%,${directionY * positionY / (paramСoefficientY / 10)}%,0) rotate(0.02deg);`;
-                    requestAnimationFrame(setMouseParallaxStyle);
-                }
-                function mouseMoveParalax(wrapper = window) {
-                    wrapper.addEventListener("mousemove", (function(e) {
-                        const offsetTop = el.getBoundingClientRect().top + window.scrollY;
-                        if (offsetTop >= window.scrollY || offsetTop + el.offsetHeight >= window.scrollY) {
-                            const parallaxWidth = window.innerWidth;
-                            const parallaxHeight = window.innerHeight;
-                            const coordX = e.clientX - parallaxWidth / 2;
-                            const coordY = e.clientY - parallaxHeight / 2;
-                            coordXprocent = coordX / parallaxWidth * 100;
-                            coordYprocent = coordY / parallaxHeight * 100;
-                        }
-                    }));
-                }
-            }));
-        }
-        setLogging(message) {
-            this.config.logging ? FLS(`[PRLX Mouse]: ${message}`) : null;
-        }
-    }
-    flsModules.mousePrlx = new MousePRLX({});
     let gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
         const targetBlockElement = document.querySelector(targetBlock);
         if (targetBlockElement) {
